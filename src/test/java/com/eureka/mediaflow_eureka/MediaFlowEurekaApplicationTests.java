2f65a189776f738310770f1215269eaa30bfb2de
Jesus <Jesus_Bh19@outlook.com>
2025-11-26 20:45:20 -0600

    Initial commit of MediaFlow Eureka Server

---
+++ src/test/java/com/eureka/mediaflow_eureka/MediaFlowEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.mediaflow_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaFlowEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
